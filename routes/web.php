abd0c4ad5d3538bd7a718a84ffc00176cb698d2e
Vanel <vanelnwaba@gmail.com>
2023-10-18 17:04:17 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\http\Controllers\HomeController::class, 'index']);
+
+Route::get('/properties', [\App\http\Controllers\PropertyController::class, 'index'])->name('property.index');
+Route::get('/properties/{slug}-{property}', [\App\http\Controllers\PropertyController::class, 'show'])->name('property.show')->where([
+    'property'=> '[0-9]+',
+    'slug'=> '[0-9a-z\-]+'
+]
+);
+
+Route::post('/properties/{property}/contact', [\App\http\Controllers\PropertyController::class, 'contact'])->name('property.contact')->where([
+    'property'=> '[0-9]+'
+]
+);;
 
 Route::prefix('admin')->name('admin.')->group(function(){
    route::resource('property', \App\http\Controllers\Admin\PropertyController::class)->except(['show']);
